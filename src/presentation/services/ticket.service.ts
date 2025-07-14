96a40972a562f6592ca3f55c27a45a1e9698f9b4
Gabriel Peña <gapena@ri.gob.do>
2025-07-14 16:43:33 +0200

    Public

--- src/presentation/services/ticket.service.ts
+++ src/presentation/services/ticket.service.ts
@@ -2,7 +2,8 @@ import { UuidAdapter } from "../../config/uuid.adapter";
 import { Ticket } from '../../domain/interfaces/tickets';
 
 export class TicketService {
-    private readonly tickets: Ticket[] = [
+
+    public readonly tickets: Ticket[] = [
         { id: UuidAdapter.v4(), number: 1, createdAt: new Date(), done: false },
         { id: UuidAdapter.v4(), number: 2, createdAt: new Date(), done: false },
         { id: UuidAdapter.v4(), number: 3, createdAt: new Date(), done: false },
@@ -11,11 +12,17 @@ export class TicketService {
         { id: UuidAdapter.v4(), number: 6, createdAt: new Date(), done: false }
     ];
 
+    private readonly workingOnTickets: Ticket[] = [];
+
     public get pendingTickets(): Ticket[] {
         return this.tickets.filter( ticket => !ticket.handleAtDesk );
     }
 
-    public lastTicketNumber(): number {
+    public get lastWorkingOnTickets(): Ticket[] {
+        return this.workingOnTickets.splice(0, 4);
+    }
+
+    public get lastTicketNumber(): number {
         return this.tickets.length > 0 ? this.tickets.at(-1)!.number : 0;
     }
 
@@ -23,7 +30,7 @@ export class TicketService {
 
         const ticket: Ticket = {
             id: UuidAdapter.v4(),
-            number: this.lastTicketNumber() + 1,
+            number: this.lastTicketNumber + 1,
             createdAt: new Date(),
             done: false,
             handleAt: undefined,
@@ -43,6 +50,8 @@ export class TicketService {
 
         ticket.handleAtDesk = desk;
         ticket.handleAt = new Date();
+
+        this.workingOnTickets.unshift({ ...ticket });
 
         return { status: 'ok', ticket };
     }
