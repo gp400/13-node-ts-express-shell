7f3c49e5eaec2a82c01b0276c7b3d86ad052bb01
Gabriel Peña <gapena@ri.gob.do>
2025-07-21 18:41:38 +0200

    Hide and show

--- src/presentation/services/ticket.service.ts
+++ src/presentation/services/ticket.service.ts
@@ -9,12 +9,12 @@ export class TicketService {
     ){}
 
     public readonly tickets: Ticket[] = [
-        { id: UuidAdapter.v4(), number: 1, createdAt: new Date(), done: false },
-        { id: UuidAdapter.v4(), number: 2, createdAt: new Date(), done: false },
-        { id: UuidAdapter.v4(), number: 3, createdAt: new Date(), done: false },
-        { id: UuidAdapter.v4(), number: 4, createdAt: new Date(), done: false },
-        { id: UuidAdapter.v4(), number: 5, createdAt: new Date(), done: false },
-        { id: UuidAdapter.v4(), number: 6, createdAt: new Date(), done: false }
+        // { id: UuidAdapter.v4(), number: 1, createdAt: new Date(), done: false },
+        // { id: UuidAdapter.v4(), number: 2, createdAt: new Date(), done: false },
+        // { id: UuidAdapter.v4(), number: 3, createdAt: new Date(), done: false },
+        // { id: UuidAdapter.v4(), number: 4, createdAt: new Date(), done: false },
+        // { id: UuidAdapter.v4(), number: 5, createdAt: new Date(), done: false },
+        // { id: UuidAdapter.v4(), number: 6, createdAt: new Date(), done: false }
     ];
 
     private readonly workingOnTickets: Ticket[] = [];
